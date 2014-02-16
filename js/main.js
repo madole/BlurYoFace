d0344330f410db5de09bc824c79917b4258f8d16
Andrew McDowell <madoliole@gmail.com>
2014-02-16 21:35:04 +1100

    Initial Commit to BlurYoFace
    App to blur your face automatically then save or upload to imgur

---
+++ js/main.js
@@ -0,0 +1,82 @@
+(function() {
+    var imageLoader = document.getElementById('imageLoader');
+    imageLoader.addEventListener('change', handleImage, false);
+    var canvas = document.getElementById('imageCanvas');
+    var ctx = canvas.getContext('2d');
+
+
+    function handleImage(e) {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            var img = new Image();
+            img.onload = function() {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
+            };
+            img.src = event.target.result;
+        };
+        reader.readAsDataURL(e.target.files[0]);
+        $("#blurFaces").fadeIn(2000);
+    }
+
+    $('#blurFaces').click(function() {
+        var $this = $(this);
+        var image = new Image();
+        image.id = 'uploadedImage';
+        image.src = $('#imageCanvas')[0].toDataURL();
+        document.getElementById('imageContent').appendChild(image);
+        var coords = $('#uploadedImage').faceDetection({
+            complete: function() {
+                console.log('The faces have been detected');
+            },
+            error: function(img, code, message) {
+                $this.text('error!');
+                alert('Error: ' + message);
+            }
+        });
+
+        for (var i = 0; i < coords.length; i++) {
+            boxBlurCanvasRGBA('imageCanvas', coords[i].x, coords[i].y, coords[i].width, coords[i].height, 30, 1);
+            $('<div>', {
+                'class': 'face',
+                'css': {
+                    'position': 'absolute',
+                    'left': coords[i].positionX + 'px',
+                    'top': coords[i].positionY + 'px',
+                    'width': coords[i].width + 'px',
+                    'height': coords[i].height + 'px'
+                }
+            }).appendTo('#content');
+        }
+
+        $('#content').fadeOut(2000, function() {
+            $('#content').html('');
+            var dataURL = $('#imageCanvas')[0].toDataURL();
+            document.getElementById('uploadedImage').src = dataURL;
+            $('#save').html("<a href='" + dataURL + "' download='blurryFaces.png' class='btn btn-default'><span class='glyphicon glyphicon-download-alt'></span> Save Image to Desktop</a>");
+            $('#save').fadeIn(2000);
+            $('#imgur').fadeIn(2000);
+
+            $('#imgur').click(function() {
+                $.ajax({
+                    url: 'https://api.imgur.com/3/image',
+                    headers: {
+                        'Authorization': 'b5924ef75ccbba7'
+                    },
+                    type: 'POST',
+                    data: {
+                        'image': dataURL
+                    }
+                }).success(function() {
+                    console.log('cool');
+                    console.log(data);
+                }).error(function(error) {
+                    console.log(error);
+                    alert('Could not reach api.imgur.com. Sorry :(');
+                });
+            });
+    });
+
+});
+})();
